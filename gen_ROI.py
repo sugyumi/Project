1e0f9704f4706f0544b288bb3a766c90a5475a66
김춘수 <50790297+kcs0421@users.noreply.github.com>
2023-08-16 10:26:54 +0900

    Update gen_ROI.py

--- gen_ROI.py
+++ gen_ROI.py
@@ -1,21 +1,25 @@
 import cv2
 import numpy as np
 import os
+import glob
 
-img = cv2.imread("view8/0810/v0810_nbfixcameramove(3)/1/1.bmp")
+images = glob.glob('2/*.bmp')
 
-h, w = img.shape[:2]
+h, w = cv2.imread(images[0]).shape[:2]
 
-for index, (i, j) in enumerate([(i, j) for i in range(3) for j in range(3)]):
-    mask = np.zeros(img.shape[:2], dtype=np.uint8)
-    roi_start = ( (w*j - 200) // (3), (h*i - 200) // (3) )
-    roi_end = ( (w*(j+1) + 200) // (3), (h*(i+1) + 200) // (3) )
+for save, fname in enumerate(images):
+    results = []
+    img = cv2.imread(fname)
+    for index, (i, j) in enumerate([(i, j) for i in range(3) for j in range(3)]):
+        mask = np.zeros(img.shape[:2], dtype=np.uint8)
+        roi_start = ( (w*j - 400) // (3), (h*i - 500) // (3) )
+        roi_end = ( (w*(j+1) + 400) // (3), (h*(i+1) + 500) // (3) )
 
-    cv2.rectangle(mask, roi_start, roi_end, 255, -1)
-    result = cv2.bitwise_and(img, img, mask=mask)
+        cv2.rectangle(mask, roi_start, roi_end, 255, -1)
+        result = cv2.bitwise_and(img, img, mask=mask)
+        results.append(result)
+        if not os.path.exists('_ROI'):
+            os.makedirs('_ROI')
 
-    if not os.path.exists('_ROI'):
-        os.makedirs('_ROI')
-
-    cv2.imwrite(f'_ROI/roi{index+1}.bmp', result)
-# os.remove('roi5.bmp')
+    cv2.imwrite(f'_ROI/roi{save}.bmp', results[save])
+    # os.remove('roi5.bmp')
