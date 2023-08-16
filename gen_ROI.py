c0b11013a79d0f1e023a8b49333d9245e4268bef
김춘수 <50790297+kcs0421@users.noreply.github.com>
2023-08-16 09:56:53 +0900

    Update gen_ROI.py

--- gen_ROI.py
+++ gen_ROI.py
@@ -2,14 +2,14 @@ import cv2
 import numpy as np
 import os
 
-img = cv2.imread("v0804_110(3)/a/7.bmp")
+img = cv2.imread("view8/0810/v0810_nbfixcameramove(3)/1/1.bmp")
 
 h, w = img.shape[:2]
 
 for index, (i, j) in enumerate([(i, j) for i in range(3) for j in range(3)]):
     mask = np.zeros(img.shape[:2], dtype=np.uint8)
-    roi_start = (w*j // (3), h*i// (3))
-    roi_end = (w*(j+1) // (3), h*(i+1) // (3))
+    roi_start = ( (w*j - 200) // (3), (h*i - 200) // (3) )
+    roi_end = ( (w*(j+1) + 200) // (3), (h*(i+1) + 200) // (3) )
 
     cv2.rectangle(mask, roi_start, roi_end, 255, -1)
     result = cv2.bitwise_and(img, img, mask=mask)
