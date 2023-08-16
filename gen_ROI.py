a6760373eb10bf24cdab5540f686d95452ce98ba
김춘수 <50790297+kcs0421@users.noreply.github.com>
2023-08-16 11:44:11 +0900

    Update gen_ROI.py

--- gen_ROI.py
+++ gen_ROI.py
@@ -3,17 +3,17 @@ import numpy as np
 import os
 import glob
 
-images = glob.glob('2/*.bmp')
+images = glob.glob('_ROI/*.bmp')
 
 h, w = cv2.imread(images[0]).shape[:2]
 
-for save, fname in enumerate(images):
+for idx, fname in enumerate(images):
     results = []
     img = cv2.imread(fname)
-    for index, (i, j) in enumerate([(i, j) for i in range(3) for j in range(3)]):
+    for i, j in [(i, j) for i in range(3) for j in range(3)]:
         mask = np.zeros(img.shape[:2], dtype=np.uint8)
-        roi_start = ( (w*j - 400) // (3), (h*i - 500) // (3) )
-        roi_end = ( (w*(j+1) + 400) // (3), (h*(i+1) + 500) // (3) )
+        roi_start = ( (w*j - 300) // (3), (h*i - 300) // (3) )
+        roi_end = ( (w*(j+1) + 300) // (3), (h*(i+1) + 300) // (3) )
 
         cv2.rectangle(mask, roi_start, roi_end, 255, -1)
         result = cv2.bitwise_and(img, img, mask=mask)
@@ -21,5 +21,4 @@ for save, fname in enumerate(images):
         if not os.path.exists('_ROI'):
             os.makedirs('_ROI')
 
-    cv2.imwrite(f'_ROI/roi{save}.bmp', results[save])
-    # os.remove('roi5.bmp')
+    cv2.imwrite(f'_ROI/roi{idx}.bmp', results[idx])
